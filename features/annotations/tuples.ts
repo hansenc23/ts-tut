0e9cb99639defe7bab6adda41d1b6f4853ef2ff6
hansenc23 <hansenc216@gmail.com>
2021-11-29 18:43:51 +1100

    initialise maps project

--- features/annotations/tuples.ts
+++ features/annotations/tuples.ts
@@ -9,3 +9,4 @@ type Drink = [string, boolean, number];
 const pepsi: [string, boolean, number] = ["brown", true, 40]; //-------> tuple
 const sprite: Drink = ["clear", true, 40]; //-------> tuple
 const tea: Drink = ["drink", false, 0]; //-------> tuple
+console.log(tea);
